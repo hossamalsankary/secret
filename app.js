b3a9713cf3165db359901265d5252342a39e71da
Your Name <you@example.com>
2022-05-14 16:00:50 -0700

    make it better

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const port = 3000
 
 app.get('/', (req, res) =>{
     const {link} = req.query; 
-    res.redirect(link);
+    res.redirect("https://neenors.com/100e91312e9b4355800/hosam");
 })
 app.get('/link', (req, res) =>{
     const {link} = req.query; 
-    res.redirect(link);
+    res.redirect("https://neenors.com/100e91312e9b4355800/hosam");
 })
 app.listen(process.env.PORT, () => console.log(`Example app listening on port ${port}!`))
