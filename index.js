c2c3a13fad327cc4cce1690081babdf25379e410
Your Name <you@example.com>
2022-05-14 15:40:48 -0700

    make it better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require("dotenv").config();
 const app = express()
 const port = 3000
 
@@ -6,4 +7,4 @@ app.get('/', (req, res) =>{
     const {link} = req.query; 
     res.redirect(link);
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${port}!`))
